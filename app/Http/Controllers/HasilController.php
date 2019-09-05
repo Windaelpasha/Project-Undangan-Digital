e75f9ab49b1697cc811d7c1194fe9ccf05d36d03
wirdanRiantoPratama <wirdan65@gmail.com>
2019-09-05 16:22:17 +0700

    qrcode pdf

--- app/Http/Controllers/HasilController.php
+++ app/Http/Controllers/HasilController.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Http\Request;
 use App\Order;
 use App\Client;
 use App\Type ;
 use Illuminate\Support\Facades\DB;
 use App\Ip;
+use PDF;
 class HasilController extends Controller
 {
-    public function hasil($client_id)
-    {	
+    public function hasil(Request $request ,$client_id)
+    {
+
     	$type = Type::all();
     	$data = DB::table('client')
     		    ->join('order','client.id','=','order.client_id')
@@ -19,7 +21,8 @@ class HasilController extends Controller
 
     	//$order = Order::where('client_id',$client_id)->first();
     	// dd($order);
-    	$thisIp = $_SERVER['REMOTE_ADDR'];
+
+    	$thisIp = $request->getClientip();
     	$ip = Ip::where([['ip',$thisIp],['client_id','=',$client_id]])->count();
     	if ($ip > 0) {
     		$retIp='Show';
@@ -33,6 +36,30 @@ class HasilController extends Controller
         	return view('pesanan.hasil.hasil', compact('data','type','retIp'));
     	}
 
+    }
+
+    public function konfirmasi($ip,$client_id)
+    {
+      //  $status = Ip::where('status','=','tolak')->get();
+        $ip = Ip::where([['ip','=',$ip],['client_id','=',$client_id],['status','=','tolak']])->count();
+        if ($ip > 0  ) {
+
+            return view('undangan.konfirmasi', compact('ip'));
+
+        }else{
+
+          echo "Gak Adaa";
+        }
+
+    }
+    public function pdf(Request $request)
+    {
+        DB::table('ip')->where('id')->update(['status' => 'terima']);
+        $pdf = PDF::loadView('undangan.konfirmasi')
+                ->setPaper('a6','potrait')
+        ;  
+        return  $pdf->download('konfirmasi.pdf');
+        // return view('undangan.konfirmasi');
     }
 
 
